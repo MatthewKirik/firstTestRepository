1dfcb07369e90398400f593593618f0315eb69ca
and_kond <and_kond@A24COMP7>
2019-03-03 13:13:50 +0200

    Add Model

--- WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WpfApp1.Models;
 
 namespace WpfApp1
 {
@@ -20,9 +21,22 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
+        public List<Mobile> mobiles = new List<Mobile>();
         public MainWindow()
         {
             InitializeComponent();
+
+            Mobile mobile = new Mobile();
+            mobiles.Add(new Mobile { Model = "S8", Price = 17500, Producer = "Samsung" });
+            mobiles.Add(new Mobile { Model = "S10", Price = 30500, Producer = "Samsung" });
+            mobiles.Add(new Mobile { Model = "X", Price = 30500, Producer = "Apple" });
+            mobiles.Add(new Mobile { Model = "S7+", Price = 30500, Producer = "Apple" });
+            mobiles.Add(new Mobile { Model = "Redmi Note 5", Price = 30500, Producer = "Xaomi" });
+            mobiles.Add(new Mobile { Model = "Lumia 8", Price = 30500, Producer = "Nokia" });
+            mobiles.Add(new Mobile { Model = "Expiria 2", Price = 30500, Producer = "Sony" });
+            mobiles.Add(new Mobile { Model = "ZenPhone MaxPro(M.2)", Price = 30500, Producer = "Asus" });
+
+
         }
     }
 }
